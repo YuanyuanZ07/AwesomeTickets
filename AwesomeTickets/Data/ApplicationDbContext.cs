d183fc745ac2e29269b4421a8bf899aabd030de6
caile <caile@BOOK-C9SGHGV43S>
2025-09-19 14:56:00 -0300

    Add project files.

---
+++ AwesomeTickets/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using AwesomeTickets.Models;
+
+namespace AwesomeTickets.Data
+{
+    public class ApplicationDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+             : base(options)
+        {
+        }
+
+        public DbSet<Event> Events { get; set; }
+    }
+}
---
+++ AwesomeTickets/Models/Event.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace AwesomeTickets.Models
+{
+    public class Event
+    {
+        public int Id { get; set; }
+
+        [Required, StringLength(100)]
+        public string Title { get; set; } = string.Empty;
+
+        [StringLength(2000)]
+        public string? Description { get; set; }
+
+        [Required, StringLength(50)]
+        public string Category { get; set; } = string.Empty;
+
+        [DataType(DataType.Date)]
+        public DateTime Date { get; set; }
+
+        [DataType(DataType.Time)]
+        public TimeSpan Time { get; set; }
+
+        [Required, StringLength(200)]
+        public string Location { get; set; } = string.Empty;
+
+        [Required, StringLength(100)]
+        public string Owner { get; set; } = string.Empty;
+
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    }
+}
